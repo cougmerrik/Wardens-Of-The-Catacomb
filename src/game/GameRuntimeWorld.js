52e7d1a48cf27ea81515aaff4d03e2b0cd916bcb
Merrik <merrik@gmail.com>
2026-03-14 10:03:37 -0500

    Rat archers!

--- src/game/GameRuntimeWorld.js
+++ src/game/GameRuntimeWorld.js
@@ -97,6 +97,10 @@ export class GameRuntimeWorld extends GameRuntimeBase {
     return spawnMimic(this, x, y);
   }
 
+  spawnRatArcher(x, y) {
+    return spawnRatArcher(this, x, y);
+  }
+
   spawnNecromancer(x, y) {
     return spawnNecromancer(this, x, y);
   }
